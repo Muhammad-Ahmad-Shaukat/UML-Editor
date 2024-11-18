77bcb933ef6634897cb9fcd34aa3cdf31329b7ed
Muhammad Ahmad <124889018+Muhammad-Ahmad-Shaukat@users.noreply.github.com>
2024-11-18 10:14:23 -0800

    change

--- src/main/java/com/boota/javaproject/UseCaseDiagramCanvasController.java
+++ src/main/java/com/boota/javaproject/UseCaseDiagramCanvasController.java
@@ -1,4 +1,39 @@
 package com.boota.javaproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class UseCaseDiagramCanvasController {
+
+    @FXML
+    private Pane canvasPane;
+
+    private Canvas canvas;
+    private GraphicsContext gc;
+
+    private String activeTool = null;
+
+    @FXML
+    public void initialize() {
+        canvas = new Canvas(canvasPane.getWidth(), canvasPane.getHeight());
+        gc = canvas.getGraphicsContext2D();
+        canvasPane.getChildren().add(canvas);
+
+    }
+
+
+
+
+    public void handleActorClick(){
+
+    }
 }
