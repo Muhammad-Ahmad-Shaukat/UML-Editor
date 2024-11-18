69e3d319f55246f219266052c040a4545fa96b2c
Muhammad Ahmad <124889018+Muhammad-Ahmad-Shaukat@users.noreply.github.com>
2024-11-18 08:54:13 -0800

    change

--- src/main/java/com/boota/javaproject/Association.java
+++ src/main/java/com/boota/javaproject/Association.java
@@ -1,37 +1,40 @@
 package com.boota.javaproject;
 
+public class Association{
 
     Multiplicity startMultiplicity;
     Multiplicity endMultiplicity;
     String text;
 
-
-        this.startMultiplicity = startMultiplicity;
-        this.endMultiplicity = endMultiplicity;
-        this.text = text;
-    }
-
-    public Multiplicity getStartMultiplicity() {
-        return startMultiplicity;
-    }
-
-    public void setStartMultiplicity(Multiplicity startMultiplicity) {
-        this.startMultiplicity = startMultiplicity;
-    }
-
-    public Multiplicity getEndMultiplicity() {
-        return endMultiplicity;
-    }
-
-    public void setEndMultiplicity(Multiplicity endMultiplicity) {
-        this.endMultiplicity = endMultiplicity;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
+Association(Multiplicity startMultiplicity, Multiplicity endMultiplicity, String text) {
+    this.startMultiplicity = startMultiplicity;
+    this.endMultiplicity = endMultiplicity;
+    this.text = text;
 }
+
+
+public Multiplicity getStartMultiplicity() {
+    return startMultiplicity;
+}
+
+public void setStartMultiplicity(Multiplicity startMultiplicity) {
+    this.startMultiplicity = startMultiplicity;
+}
+
+public Multiplicity getEndMultiplicity() {
+    return endMultiplicity;
+}
+
+public void setEndMultiplicity(Multiplicity endMultiplicity) {
+    this.endMultiplicity = endMultiplicity;
+}
+
+public String getText() {
+    return text;
+}
+
+public void setText(String text) {
+    this.text = text;
+}
+}
+
